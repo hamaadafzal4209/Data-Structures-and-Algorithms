e623e16e37d37cf536df949260d0e69a1b4dd8ea
Hamaad Afzal <155874874+hamaadafzal4209@users.noreply.github.com>
2024-06-06 11:13:18 +0500

    Time: 0 ms (100.00%) | Memory: 9.8 MB (59.47%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int highestAltitude = 0;
+        int currentAltitude = 0;
+
+        for (int i = 0; i < gain.size(); i++) {
+            currentAltitude += gain[i];
+
+            if (currentAltitude > highestAltitude) {
+                highestAltitude = currentAltitude;
+            }
+        }
+        return highestAltitude;
+    }
+};
